096e4fd3df06e245250d14e730ee94689ce8bc17
Saumyaranjan <saumyaranjan.sp20@gmail.com>
2021-11-02 16:57:04 +0530

    refresh token

--- server/index.js
+++ server/index.js
@@ -17,26 +17,75 @@ const users = [
     isAdmin: false,
   },
 ];
+let refreshTokens = [];
+app.post("/server/refresh", (req, res) => {
+  //take refresh token from user
+  const refreshToken = req.body.token;
+  //send error if there is no token or it's invalid
+  if (!refreshToken) {
+    return res.status(401).json("you are not authenticated");
+  }
+  if (!refreshTokens.includes(refreshToken)) {
+    return res.status(403).json("refresh token is not valid");
+  }
 
+  //if everything is ok , create new access token, refresh token and send to user
+  jwt.verify(refreshToken, "myrefreshsecretkey", (err, user) => {
+    err && console.log(err);
+    refreshTokens = refreshTokens.filter((token) => token !== refreshToken);
+
+    const newAccessToken = generateAccessToken(user);
+    const newRefreshToken = generateRefreshToken(user);
+
+    refreshTokens.push(newRefreshToken);
+    res.status(200).json({
+      accessToken: newAccessToken,
+      refreshToken: newRefreshToken,
+    });
+  });
+});
+
+const generateAccessToken = (user) => {
+  return jwt.sign(
+    { id: user.id, isAdmin: user.isAdmin },
+    "mysecretkey",
+    { expiresIn: "20s" }
+    //Token expires in 20s so no-one else can delete account rather than the user itselfb but user also have to login after 20 second so we use refresh token
+  );
+};
+
+const generateRefreshToken = (user) => {
+  return jwt.sign(
+    { id: user.id, isAdmin: user.isAdmin },
+    "myrefreshsecretkey",
+    { expiresIn: "20s" }
+    //Refresh token generated so after 20 s only access token change but user wouldnt be logout
+  );
+};
+
+//login from jwt
 app.post("/server/login", (req, res) => {
   const { username, password } = req.body;
   const user = users.find((u) => {
     return u.username === username && u.password === password;
   });
   if (user) {
-    const accessToken = jwt.sign(
-      { id: user.id, isAdmin: user.isAdmin },
-      "mysecretkey"
-    );
+    const accessToken = generateAccessToken(user);
+    const refreshToken = generateRefreshToken(user);
+    refreshTokens.push(refreshToken);
+
     res.json({
       username: user.username,
       isAdmin: user.isAdmin,
       accessToken,
+      refreshToken,
     });
   } else {
     res.status(400).json("Username or password incorrect");
   }
 });
+
+//verify if admin or not so it can delete accounts or not
 
 const verify = (req, res, next) => {
   const authHeader = req.headers.authorization;
@@ -55,6 +104,8 @@ const verify = (req, res, next) => {
     res.status(401).json("you are not authenticated");
   }
 };
+
+//if admin then only can delete the accounts
 
 app.delete("/server/users/:userId", verify, (req, res) => {
   if (req.user.id === req.params.userId || req.user.isAdmin) {
