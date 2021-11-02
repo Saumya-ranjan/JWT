069c3b30a1b9e685336b3335245a79044f2c26e0
Saumyaranjan <saumyaranjan.sp20@gmail.com>
2021-11-02 11:59:53 +0530

    Delete learn

--- server/index.js
+++ server/index.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const app = express();
 app.use(express.json());
+const jwt = require("jsonwebtoken");
 
 const users = [
   {
     id: "1",
     username: "sam",
-    password: "Sam2002",
+    password: "sam2002",
     isAdmin: true,
   },
   {
@@ -23,9 +24,43 @@ app.post("/server/login", (req, res) => {
     return u.username === username && u.password === password;
   });
   if (user) {
-    res.json(user);
+    const accessToken = jwt.sign(
+      { id: user.id, isAdmin: user.isAdmin },
+      "mysecretkey"
+    );
+    res.json({
+      username: user.username,
+      isAdmin: user.isAdmin,
+      accessToken,
+    });
   } else {
     res.status(400).json("Username or password incorrect");
+  }
+});
+
+const verify = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (authHeader) {
+    const token = authHeader.split(" ")[1];
+
+    jwt.verify(token, "mysecretkey", (err, user) => {
+      if (err) {
+        return res.status(403).json("Token is not valid");
+      }
+
+      req.user = user;
+      next();
+    });
+  } else {
+    res.status(401).json("you are not authenticated");
+  }
+};
+
+app.delete("/server/users/:userId", verify, (req, res) => {
+  if (req.user.id === req.params.userId || req.user.isAdmin) {
+    res.status(200).json("User has been deleted");
+  } else {
+    res.status(403).json("you are not allowed to delete this user");
   }
 });
 
